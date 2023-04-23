7dff33404796dd34748313501e602631a28eeb09
Siddhesh Surve <sysurve24@gmail.com>
2023-04-23 06:10:26 +0000

    added memesData instead api + eventHandling

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import memesData from "../memesData"
 import "./Meme.css"
 
 function Meme() {
+
+    let url //as used in two functions global declaration
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        // created array then created index in between array.lenght() i.e 100
+        url = memesArray[randomNumber].url
+        // or const {url} = memesArray[randomNumber]
+        console.log(url);
+    }
+
   return (
     <main>
-        <form className='form'>
+        <div className='form'>
             <input 
                 type='text'
                 placeholder='Top Text' 
@@ -15,10 +28,13 @@ function Meme() {
                 placeholder='Bottom Text'
                 className='form--input'
             />
-            <button className='form--button'>
+            <button
+                onClick={getMemeImage}
+                className='form--button'
+            >
                 Get a new meme image 🖼
             </button>
-        </form>
+        </div>
     </main>
   )
 }
