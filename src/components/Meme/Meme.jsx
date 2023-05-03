678e69666eae570fe6b2010cbd7c0ea01bf1f94b
Siddhesh Surve <sysurve24@gmail.com>
2023-05-03 18:20:52 +0000

    used useState to set new image url

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -1,42 +1,28 @@
-import React from 'react'
-// import memesData from "../memesData"
-import "./Meme.css"
+import React, { useState } from "react";
+import memesData from "../memesData";
+import "./Meme.css";
 
 function Meme() {
+  const [memeImage, setMemeImage] = useState("");
 
-    // let url //as used in two functions global declaration
-
-    // function getMemeImage() {
-    //     const memesArray = memesData.data.memes;
-    //     const randomNumber = Math.floor(Math.random() * memesArray.length)
-    //     // created array then created index in between array.lenght() i.e 100
-    //     url = memesArray[randomNumber].url
-    //     // or const {url} = memesArray[randomNumber]
-    //     console.log(url);
-    // }
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
 
   return (
     <main>
-        <div className='form'>
-            <input 
-                type='text'
-                placeholder='Top Text' 
-                className='form--input'
-            />
-            <input 
-                type='text' 
-                placeholder='Bottom Text'
-                className='form--input'
-            />
-            <button
-                // onClick={getMemeImage}
-                className='form--button'
-            >
-                Get a new meme image 🖼
-            </button>
-        </div>
+      <div className="form">
+        <input type="text" placeholder="Top Text" className="form--input" />
+        <input type="text" placeholder="Bottom Text" className="form--input" />
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={memeImage} className="meme--image" alt="memeImage" />
     </main>
-  )
+  );
 }
 
-export default Meme
+export default Meme;
